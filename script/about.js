02c9f1f5e94e9a4429b34c1a6fce9b1354aebc00
Maya Pavlova <92288530+Lone-Firefly@users.noreply.github.com>
2025-02-21 18:17:54 +0300

    feat: "applied fixes to the About page"

---
+++ script/about.js
@@ -0,0 +1,10 @@
+const btnPopup = document.querySelector(".lic__btn-popup");
+const table = document.querySelector(".lic__card");
+
+function popupMenu() {
+  table.classList.toggle("lic__card_hide");
+}
+
+btnPopup.addEventListener("click", function () {
+  popupMenu();
+});
